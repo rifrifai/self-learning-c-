60600d0e4dd0304c000543453b9a34d5fe7de2b7
rifaiabdurrohman <rifaiabdurrohman8@gmail.com>
2025-12-01 09:43:02 +0700

    feat: fizzbuzz

--- soal1/obj/Debug/net9.0/soal1.AssemblyInfo.cs
+++ soal1/obj/Debug/net9.0/soal1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("soal1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99d98fd7cfc80951f1dbf14d4142ba206197f002")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc067d249af5e7fc4230a98c309fb5f6a323f9c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("soal1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("soal1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
