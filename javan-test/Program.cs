e6a1e14d06a63f05dcd274a0dc9b142ce8f075b3
rifaiabdurrohman <rifaiabdurrohman8@gmail.com>
2025-12-01 10:21:25 +0700

    feat: linq

--- javan-test/Program.cs
+++ javan-test/Program.cs
@@ -1,18 +1,48 @@
-﻿for (int i = 1; i <= 100; i++)
+﻿class Program
 {
-    if(i % 5 == 0 && i % 3 == 0)
+    static void Main(string[] args)
     {
-        Console.Write("FizzBuzz");
+        FizzBuzz();
+        IsPalindrome("bagas");
+        LinqBasic();
     }
-    else if(i % 5 == 0)
+
+    static void FizzBuzz()
     {
-        Console.Write("Fizz");
-    } else if (i % 3 == 0)
-    {
-        Console.Write("Buzz");
-    } else
-    {
-        Console.Write(i);
+        int n = 55;
+        for (int i = 1; i <= n; i++)
+        {
+            if(i % 5 == 0 && i % 3 == 0)
+            {
+                Console.Write("FizzBuzz");
+            }
+            else if(i % 5 == 0)
+            {
+                Console.Write("Fizz");
+            } else if (i % 3 == 0)
+            {
+                Console.Write("Buzz");
+            } else
+            {
+                Console.Write(i);
+            }
+            if(i < n) Console.Write(", ");
+        }
+        Console.WriteLine();
     }
-    if(i > 0) Console.Write(", ");
+
+    static void IsPalindrome(string text)
+    {
+        var reserved = new string(text.Reverse().ToArray());
+        Console.WriteLine(text.Equals(reserved, StringComparison.OrdinalIgnoreCase));
+    }
+
+    static void LinqBasic()
+    {
+        List<int> numbers = [1, 5, 8, 10, 3, 40, 33, 21, 19];
+        // var result = numbers.Where(n => n % 2 == 0).OrderBy(n => n).ToList();
+        var result = numbers.Where(n => n % 2 == 1).OrderBy(n => n);
+
+        Console.WriteLine(string.Join(", ", result));
+    }
 }
